a7cdf65a4d9ded225bf99edd25e2e6009f9a65d2
Maayan Ishlach <maayanishlach@MacBook-Air-sl-Maayan.local>
2024-11-06 15:53:50 +0200

    try

---
+++ Demo0.java
@@ -0,0 +1,8 @@
+// Prints the numbers 0 to 5.
+// (Same program as shown in lecture 1-1).
+public class Demo0 {
+	public static void main(String[] args) {
+        int i = 0;
+        System.out.println(i);
+    }
+ }
