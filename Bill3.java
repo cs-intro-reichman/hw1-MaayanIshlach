306cc2e676d9a82eba7c1661d454bcf0a70dd772
Maayan Ishlach <maayanishlach@gmail.com>
2024-11-07 16:43:31 +0200

    try1

--- Bill3.java
+++ Bill3.java
@@ -6,11 +6,8 @@ public class Bill3 {
 		String name2=args[1];
 		String name3=args[2];
 		int total_price = Integer.parseInt(args[3]);
-		//ystem.out.println("the names are:"+ name1+","+name2+","+name3+" and the total price is: "+total_price);
-
 		Double temp;
 		temp=Math.ceil(total_price/3.0);
-// Dear Dan, Lisa, and Ron: pay 34.0 Shekels each.
-		System.out.println("Dear "+name1+","+name2+","+name3+": pay "+temp+" each.");
+		System.out.println("Dear "+name1+", "+name2+", "+name3+": pay "+temp+" Shekels each");
 	}
 }
