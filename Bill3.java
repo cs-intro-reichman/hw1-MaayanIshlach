4ea943255f47b0f29785d4bc7d432420b414a4ce
Maayan Ishlach <maayanishlach@gmail.com>
2024-11-07 11:46:46 +0200

    testing2

--- Bill3.java
+++ Bill3.java
@@ -6,7 +6,7 @@ public class Bill3 {
 		String name2=args[1];
 		String name3=args[2];
 		int total_price = Integer.parseInt(args[3]);
-		System.out.println("the names are:"+ name1+","+name2+","+name3+" and the total price is: "+total_price);
+		//ystem.out.println("the names are:"+ name1+","+name2+","+name3+" and the total price is: "+total_price);
 
 		Double temp;
 		temp=Math.ceil(total_price/3.0);
